37396ce80e1bdd192578c2e1d1658159e4dcbf2c
anhcuong <trananhcuong1991@gmail.com>
2020-01-18 14:01:42 +0700

    Update model

--- pipeline/config.py
+++ pipeline/config.py
@@ -16,6 +16,7 @@ openpose_model_folder = os.getenv('OPENPOSE_MODEL_FOLDER', 'C:/Users/Frank/works
 
 # CNN
 cnn_model_location = os.getenv('CNN_MODEL_LOCATION', os.path.join(cur_dir, 'models', 'hmnn_full_best_weights.hdf5'))
+cnn_model_location = os.getenv('CNN_MODEL_LOCATION', os.path.join(cur_dir, 'models', 'full_model_15012020.h5'))
 cnn_frames_per_prediction = 5
 
 # Crowding
