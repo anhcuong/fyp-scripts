0cc41bc00f7de6a8c3db770f86aa59be2eae3f2f
frank <frank.tran@datarobot.com>
2020-01-18 14:06:50 +0700

    update process_images

--- pipeline/process_images.py
+++ pipeline/process_images.py
@@ -84,9 +84,9 @@ def on_created_custom(event):
     new_frame_idx = re.match('[\w]*frame(?P<new_frame_idx>[0-9]{3})[\w]*',
         file_name).group('new_frame_idx')
 
-    print('Folder updated : {}\n'.format(folder_updated)
-          'Folder to check : {}\n'.format(folder_check)
-          'New File Frame ID : {new_frame_idx}\n'.format(new_frame_idx)
+    print('Folder updated : {}\n'.format(folder_updated) +
+          'Folder to check : {}\n'.format(folder_check) +
+          'New File Frame ID : {new_frame_idx}\n'.format(new_frame_idx) +
           'File Name : {file_name}'.format(file_name))
 
     DEQUE[folder_updated].append(new_frame_idx)
