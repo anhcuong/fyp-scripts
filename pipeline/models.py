dd47c87dedf7391c58e034103b5d5871d60503f8
frank <frank.tran@datarobot.com>
2020-01-18 14:40:28 +0700

    Test

--- pipeline/models.py
+++ pipeline/models.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import cv2
 # from keras.models import load_model
-from tensorflow.keras.model import load_model
+from tensorflow.keras.models import load_model
 from keras.backend.tensorflow_backend import set_session
 from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
